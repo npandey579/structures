1c3728eb922cb1403b1192fc0a1b6815639d1f8c
Nitesh <npandey579@gmail.com>
2020-02-09 22:16:08 +0530

    first

---
+++ DSTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using DataStructures.Graph;
+using System;
+
+namespace DSTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var la = new Vertex<string>("Los Angeles");
+            var sf = new Vertex<string>("San Francisco");
+            var lv = new Vertex<string>("Las Vegas");
+            var se = new Vertex<string>("Seattle");
+            var au = new Vertex<string>("Austin");
+            var po = new Vertex<string>("Portland");
+
+            var testGraph = new UndirectedGenericGraph<string>();
+
+            // la <=> sf, lv, po
+            testGraph.AddPair(la, sf);
+            testGraph.AddPair(la, lv);
+            testGraph.AddPair(la, po);
+
+            // sf <=> se, po
+            testGraph.AddPair(sf, se);
+            testGraph.AddPair(sf, po);
+
+            // lv <=> au
+            testGraph.AddPair(lv, au);
+
+            // se <=> po
+            testGraph.AddPair(se, po);
+
+            // Check to see that all neighbors are properly set up
+            foreach (var vertex in testGraph.Vertices)
+            {
+                System.Diagnostics.Debug.WriteLine(vertex.ToString());
+            }
+
+            // Test searching algorithms
+            System.Diagnostics.Debug.WriteLine("D F S result");
+            //testGraph.DepthFirstSearch(la, (m) => System.Diagnostics.Debug.WriteLine(m));
+
+            System.Diagnostics.Debug.WriteLine("B F S result");
+            testGraph.BreadthFirstSearch(la, (m) => System.Diagnostics.Debug.WriteLine(m));
+        }
+    }
+}
---
+++ DataStructures/Graph/DFSIterator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructures.Graph
+{
+    public class DFSIterator<T>
+    {
+        private readonly Vertex<T> root;
+        private readonly HashSet<Vertex<T>> visited = new HashSet<Vertex<T>>();
+        public DFSIterator(Vertex<T> rootVertex)
+        {
+            root = rootVertex;
+        }
+
+        public IEnumerable<Vertex<T>> Iterate()
+        {
+            visited.Clear();
+
+            return DepthFirstSearch(root, visited);
+        }
+
+        private IEnumerable<Vertex<T>> DepthFirstSearch(Vertex<T> vertex, HashSet<Vertex<T>> visited)
+        {
+            if (visited.Contains(vertex))
+                yield break;
+
+            visited.Add(vertex);
+
+            yield return vertex;
+
+            foreach (Vertex<T> neighbor in vertex.Neighbors.SelectMany(n => DepthFirstSearch(n, visited)))
+                yield return neighbor;
+        }
+    }
+}
